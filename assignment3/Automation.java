f1de5f916c4062cf31ec41a7ba3faa54afc0f959
Soniyaloganathan <137623579+Soniyaloganathan@users.noreply.github.com>
2023-07-10 15:47:42 +0530

    Add files via upload

---
+++ assignment3/Automation.java
@@ -0,0 +1,14 @@
+package assignment3;
+
+public class Automation extends MultipleLangauge  {
+
+	public static void main(String[] args) {
+		Automation au=new Automation();
+		au.Java();
+		au.python();
+		au.ruby();
+		au.Selenium();
+
+	}
+
+}
---
+++ assignment3/MultipleLangauge.java
@@ -0,0 +1,24 @@
+package assignment3;
+
+public class MultipleLangauge implements Language{
+
+	public void Java() {
+		System.out.println("automate java");
+		
+	}
+
+	public void Selenium() {
+		
+		System.out.println("automate selenium");
+	}
+	
+	public void python() {
+		System.out.println("automate python");
+
+	}
+	public void ruby() {
+		System.out.println("automate ruby");
+
+	}
+
+}
