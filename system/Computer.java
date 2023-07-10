92df8d617ab3fa6dc43b6e7cf5ab07fefde818c3
Soniyaloganathan <137623579+Soniyaloganathan@users.noreply.github.com>
2023-07-10 15:36:07 +0530

    Add files via upload

---
+++ system/Computer.java
@@ -0,0 +1,9 @@
+package org.system;
+
+public class Computer {
+
+	public void computerModel() {
+		System.out.println("Please print the computerModel number-->parent as a single inheritance");
+
+	}
+}
---
+++ system/Desktop.java
@@ -0,0 +1,15 @@
+package org.system;
+
+public class Desktop extends Computer  {
+
+	public void desktopSize() {
+		System.out.println("please print the desktopsize number");
+
+	}
+	public static void main(String[] args) {
+		Desktop dt =new Desktop();
+		dt.desktopSize();
+		dt.computerModel();
+	}
+	
+}
